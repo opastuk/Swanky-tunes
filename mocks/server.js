8793ad1e6fb7863f8500d69e29885a231b895e3f
Никита Павленко <nick.pavlenko1998@gmail.com>
2019-11-17 22:50:01 +0300

    feat: добавлены моки для карточек с треками

--- mocks/server.js
+++ mocks/server.js
@@ -7,9 +7,24 @@ const init = async () => {
   });
   server.route({
     method: 'GET',
-    paths: 'music',
-    handler: (request, h) => ([]),
-
+    paths: '/music',
+    handler: (request, h) => ([
+      {
+        id: 1,
+        poster: 'https://avatars.yandex.net/get-music-content/113160/254ab226.a.8264759-1/200x200',
+        track: 'https://music.yandex.ru/album/8264759/track/56055721',
+      },
+      {
+        id: 2,
+        poster: 'https://avatars.yandex.net/get-music-content/113160/0b082e28.a.5643859-1/200x200',
+        track: 'https://music.yandex.ru/album/5643859/track/42662275',
+      },
+      {
+        id: 3,
+        poster: 'https://avatars.yandex.net/get-music-content/163479/13523d43.a.5208207-2/200x200',
+        track: 'https://music.yandex.ru/album/5208207/track/41779891',
+      },
+    ]),
   });
 
   await server.start();
