9de086d329be679891be1409bcc302c3148167b6
Никита Павленко <nick.pavlenko1998@gmail.com>
2020-03-19 22:41:05 +0300

    добавлена новая фотография на странице home

--- src/components/mainLogo.vue
+++ src/components/mainLogo.vue
@@ -1,14 +1,7 @@
 <template>
- <div>
-   <div class="main-logo">
-     <div class="main-logo__background">
-       <img class="main-logo__texture" src="../assets/img/logo-bg.png" alt="texture">
-       <div class="main-logo__flare"></div>
-       <div class="main-logo__img-container">
-         <img class="main-logo__img" src="../assets/img/logo.png" alt="Swanky Tunes" width="760">
-       </div>
-     </div>
-   </div>
+ <div class="main-logo">
+    <img class="main-logo__img" src="../assets/img/pic.png"
+         alt="Swanky Tunes" width="1100" height="656">
  </div>
 </template>
 
@@ -21,44 +14,4 @@ export default class mainLogo extends Vue {
 </script>
 
 <style scoped lang="less">
-  @media (min-width: 768px) {
-    .main-logo {
-      width: 100%;
-    }
-    .main-logo__background {
-      width: 750px;
-      height: 400px;
-      margin: 0 auto;
-      position: relative;
-      overflow: hidden;
-    }
-    .main-logo__texture {
-      width: 100%;
-    }
-    .main-logo__flare {
-      width: 90px;
-      height: 520px;
-      background-color: #ffffff;
-      position: absolute;
-      transform: rotate(20deg) translateY(-50%);
-      animation: timing 3s infinite;
-      top: 50%;
-      left: -190px;
-      z-index: 2;
-    }
-    @keyframes timing {
-      100%{
-        left: 100%;
-        margin-left: 1%;
-      }
-    }
-    .main-logo__img {
-      position: absolute;
-      top: -100px;
-      left: 50%;
-      transform: translateX(-50%);
-      z-index: 3;
-    }
-
-  }
 </style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,11 +1,8 @@
 <template>
   <div class="home">
-    <div class="container home__wrapper">
-      <h1 class="visually-hidden">Swanky Tunes</h1>
-      <navMenu></navMenu>
-      <mainLogo class="main-logo"></mainLogo>
-      <footerMenu></footerMenu>
-    </div>
+    <navMenu/>
+    <mainLogo/>
+    <footerMenu/>
   </div>
 </template>
 
@@ -23,20 +20,4 @@ export default class Home extends Vue {
 </script>
 
 <style scoped lang="less">
-  .home {
-    background-color: #000000;
-    width: 100%;
-    height: 100vh;
-  }
-  .home__wrapper {
-    .flex-layout();
-    min-height: 550px;
-  }
-  .container {
-    @media (min-width: 768px) {
-      width: 760px;
-      height: 100vh;
-      margin: 0 auto;
-    }
-  }
 </style>
