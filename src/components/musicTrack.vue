508c494f67e748112aa351c060fd2c33a00781db
Никита Павленко <nick.pavlenko1998@gmail.com>
2020-05-17 20:21:31 +0300

    feat: пропущена точка с запятой

--- src/components/musicTrack.vue
+++ src/components/musicTrack.vue
@@ -119,7 +119,7 @@ export default class musicTrack extends Vue {
     	if (!this.player.paused) {
     		this.player.pause();
     	} else if (this.player.paused && this.player.readyState && this.player.play() !== undefined) {
-    		this.player.play()
+    		this.player.play();
     	}
     	this.player.onplaying = () => {
     		this.isPausedNow = false;
